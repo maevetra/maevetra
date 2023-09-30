18e2fb92859f868564cb6bafbce8fc822d0ba8dc
Maeve Tra <ntra@purdue.edu>
2023-09-30 13:12:38 -0400

    next config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@
 const nextConfig = {}
 
 module.exports = {
+    output: 'export',
     webpack: (config, options) =>
     {
         config.module.rules.push({
             test: /\.pdf$/i,
             type: 'asset/source'
         })
+        
 
         return config
     },
